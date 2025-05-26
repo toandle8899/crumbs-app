7535e7ced736ae5c82f25af1b0d20ac4759427f1
Duc Toan Le <duc-toan.le@hsrw.org>
2025-05-26 10:42:47 +0200

    Update base path for crumbs-app GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Crumbs-AI-App/',
+  base: '/crumbs-app/',
   build: {
     outDir: 'dist',
     sourcemap: false,
